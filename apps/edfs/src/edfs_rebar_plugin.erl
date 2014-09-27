d918b18a4072871edc962eaf5b5c7da7a7757b42
Mawuli Adzaku <mawuli.ypa@gmail.com>
2014-09-28 00:40:06 +0300

    Add config parsing to the edfs_rebar plugin

--- apps/edfs/src/edfs_rebar_plugin.erl
+++ apps/edfs/src/edfs_rebar_plugin.erl
@@ -9,9 +9,35 @@
 -module(edfs_rebar_plugin).
 
 -export([
+    parse_config/2,
     config_gen/2
 ]).
 
+%% @doc Parses the edfs.conf and generates Erlang app.config
+parse_config(Config, _Reltoolfile) ->
+    SchemaFile = rebar_config:get_local(Config, edfs_schema,"edfs.schema"),
+    ConfigFile = rebar_config:get_local(Config, edfs_conf, "edfs.conf"),
+    Schema = cuttlefish_schema:files([SchemaFile]),
+    Conf = cuttlefish_conf:files([ConfigFile]),
+
+    %% now parse the conf file
+    NewConfig = case cuttlefish_generator:map(Schema, Conf) of
+        {error, Phase, {error, Errors}} ->
+            lager:error("Error generating configuration in phase ~s", [Phase]),
+            _ = [ cuttlefish_error:print(E) || E <- Errors],
+            init:stop(1);
+        ValidConfig -> ValidConfig
+    end,
+
+    Destination  = rebar_config:get_local(Config, edfs_app_configpath,"edfs.sys.config"),
+    case file:write_file(Destination, io_lib:fwrite("~p.\n", [NewConfig])) of
+        ok ->
+            io:format("==> sys config written to ~p ~n", [Destination]);
+        Error ->
+            io:format("==> Could not parse configuration file: ~p ~n", [Error]),
+            ok
+    end.
+    
 %% @doc Run this via rebar (rebar config_gen) to generate the .conf files on ./priv
 config_gen(Config, _Reltoolfile) ->
     generate_config_files(Config).
@@ -19,17 +45,17 @@ config_gen(Config, _Reltoolfile) ->
 %% @doc Main configuration files generator. Translates the Schema files to .conf
 %% files
 generate_config_files(Config) ->       
-    io:format("===> generating eDFS config file ~n", []),
+    io:format("==> generating eDFS config file ~n", []),
     EDFS_schema_file = rebar_config:get_local(Config, edfs_schema,"edfs.schema"),
     EDFS_config_file = rebar_config:get_local(Config, edfs_conf, "edfs.conf"),
     generate_conf(EDFS_schema_file, EDFS_config_file),
 
-    io:format("===> generating Erlang VM config file ~n", []),
+    io:format("==> generating Erlang VM config file ~n", []),
     EDFS_vmschema_file = rebar_config:get_local(Config, edfs_vmschema, "erlang_vm.schema"),
     EDFS_vmconfig_file = rebar_config:get_local(Config, edfs_vmconf, "erlang_vm.conf"),
     generate_conf(EDFS_vmschema_file, EDFS_vmconfig_file),
     
-    io:format("===> finished generating config files ~n"),
+    io:format("==> finished generating config files ~n", []),
     ok.
     
 %% @doc Generate .conf files form the .schema files
