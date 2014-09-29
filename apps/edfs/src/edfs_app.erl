9cd6396b2900c0f82f4d02e3f8913edfa4781e7e
Mawuli Adzaku <mawuli.ypa@gmail.com>
2014-09-29 07:47:49 +0300

    Add application startpoint.
    This sets up the connection listener

--- apps/edfs/src/edfs_app.erl
+++ apps/edfs/src/edfs_app.erl
@@ -2,15 +2,34 @@
 
 -behaviour(application).
 
+-include("edfs.hrl").
+
 %% Application callbacks
 -export([start/2, stop/1]).
+
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    start_listener(tcp),
     edfs_sup:start_link().
 
 stop(_State) ->
     ok.
+
+start_listener(tcp) ->
+    PortConfig = case edfs_config:get(tcp_listener) of
+                     {_IP, Port} ->
+                         [{port, Port}];
+                     random ->
+                         []
+                 end,
+    TransportOpts = PortConfig ++ [{active, true}, {max_connections, infinity}],
+    {ok, _} = ranch:start_listener(?EDFS_TCP_LISTENER, ?EDFS_TCP_ACCEPTORS,
+                                       ranch_tcp, TransportOpts,
+                                       edfs_vnode, []),
+    Port1 = ranch:get_port(?EDFS_TCP_LISTENER),
+    ?STDOUT("EDFS started on tcp port: ~p", [Port1]),
+    ?LOG("EDFS started on tcp port: ~p", [Port1]).
